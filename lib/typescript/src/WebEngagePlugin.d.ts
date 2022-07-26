917a8ec3c941d654794abacc868bfa738d291467
Antonio Carpentieri <a.carpe@gmail.com>
2022-07-26 17:13:14 +0200

    feat: found the right builder

---
+++ lib/typescript/src/WebEngagePlugin.d.ts
@@ -0,0 +1,13 @@
+import { DestinationPlugin, IdentifyEventType, PluginType, ScreenEventType, TrackEventType, SegmentAPISettings, UpdateType } from "@segment/analytics-react-native";
+export declare class WebEngagePlugin extends DestinationPlugin {
+    type: PluginType;
+    key: string;
+    private webEngage;
+    private settings;
+    private isInitialized;
+    update(settings: SegmentAPISettings, _: UpdateType): void;
+    identify(event: IdentifyEventType): IdentifyEventType;
+    track(event: TrackEventType): TrackEventType;
+    screen(event: ScreenEventType): ScreenEventType;
+    reset(): void;
+}
